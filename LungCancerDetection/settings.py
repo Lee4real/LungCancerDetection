64a5890badfc3b7e83c07d836a2f8a61796872f3
AbdoXm <treated.contact@gmail.com>
2024-04-26 18:29:43 +0100

    commit

--- LungCancerDetection/settings.py
+++ LungCancerDetection/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "abdoxm.pythonanywhere.com"]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
